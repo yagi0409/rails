a3df2e3f652552a5490721476f175b186d8a7915
青柳奏汰 <greenwillow49@gmail.com>
2022-01-13 12:49:55 +0900

    [Finish]2-10

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '147717013f8c2a821015cc7b35ac1bebed1117398ede5603b834afc3867a4ac8d54957a36f2211c8e2db94746ba5ded9afccfa4645adddabdc2ed653eb836232'
